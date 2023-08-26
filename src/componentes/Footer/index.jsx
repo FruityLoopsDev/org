bc0d3f481f4677dfc47c5d19986cb9cc539202b2
Dall-ECruz <cesarcruztrader@gmail.com>
2023-08-26 11:16:38 -0400

    footer_png

--- src/componentes/Footer/index.jsx
+++ src/componentes/Footer/index.jsx
@@ -1,6 +1,6 @@
-import footerbg from "../../img/footer.png";
+import "./Footer.css";
 const Footer = () => {
-    return <footer className="footer" style={{ backgroundImage: 'url(${footerbg})' }}>
+    return <footer className="footer" style={{ backgroundImage: "url(/img/footer.png)" }}>
         <div className="redes">
             <a href='https://www.aluracursos.com/'>
                 <img src="/img/facebook.png" alt="Facebook" />
