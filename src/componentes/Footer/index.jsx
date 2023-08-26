69296bc543ff49976d558f8f4c50d07921cd1ced
Dall-ECruz <cesarcruztrader@gmail.com>
2023-08-26 08:25:56 -0400

    Actualizando el index.html

---
+++ src/componentes/Footer/index.jsx
@@ -0,0 +1,21 @@
+import "./Footer.css";
+const Footer = () => {
+    return <footer className="footer" style={{ backgroundImage: "url(/img/Footer.png)" }}>
+        <div className="redes">
+            <a href='https://www.aluracursos.com/'>
+                <img src="/img/facebook.png" alt="Facebook" />
+            </a>
+            <a href='https://www.aluracursos.com/'>
+                <img src="/img/twitter.png" alt="twitter" />
+            </a>
+            <a href='https://www.aluracursos.com/'>
+                <img src="/img/instagram.png" alt="instagram" />
+            </a>
+        </div>
+        <a href='https://www.aluracursos.com/'>
+            <img src='/img/Logo.png' alt='org' />
+        </a>
+        <strong>Desarrollado por Alura</strong>
+    </footer>;
+};
+export default Footer;
